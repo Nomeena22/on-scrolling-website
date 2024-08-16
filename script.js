b388207a6220efed2b558ba12584d6721d616297
Nomeena <nominaabbasis@gmail.com>
2024-08-16 23:07:21 +0500

    part-2

--- script.js
+++ script.js
@@ -45,7 +45,47 @@ tl.to(".overly-div",{
 tl.to(".scrolling",{
     width: "100%",
 },'a')
+var tl2 = gsap.timeline({scrollTrigger:{
+    trigger: ".part-2",
+    start:"0% 70%",
+    end:"50% 50%",
+    scrub:true,
+    markers:true,
+}})
 
+tl2.to(".wrapper-div",{
+    height:0,
+    marginTop: 0,
+})
+
+let tl3 = gsap.timeline({
+    scrollTrigger:{
+        trigger: ".content-2",
+        start:"20% 50%",
+        end: "100% 50%",
+        // markers: true,
+        scrub: 1,
+    },  
+});
+tl3.to(".content-2 .text-area-blur h1",{
+    width:"100%",
+})
+tl3.to(".content-2 .text-area-blur h2",{
+    delay: -0.4,
+    width:"100%",
+})
+// var tl2 = gsap.timeline({scrollTrigger:{
+//     trigger: ".part-2",
+//     start:"50% 50%",
+//     end:"100% 50%",
+//     scrub:true,
+//     // markers:true,
+//     pin:true,
+// }})
+// tl2.to(".wrapper-div",{
+//     height: 0,
+//     marginTop: 0,
+// })
 // var tl = gsap.timeline({scrollTrigger:{
 //     trigger : ".part-1",
 //     start : "50% 50%",
