d5ed3ce02266c0b4f976e1c989bc6d41b9b2c582
Nomeena <nominaabbasis@gmail.com>
2024-08-16 23:19:44 +0500

    part-2 gsap

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ var tl2 = gsap.timeline({scrollTrigger:{
     start:"0% 70%",
     end:"50% 50%",
     scrub:true,
-    markers:true,
+    // markers:true,
 }})
 
 tl2.to(".wrapper-div",{
@@ -74,45 +74,3 @@ tl3.to(".content-2 .text-area-blur h2",{
     delay: -0.4,
     width:"100%",
 })
-// var tl2 = gsap.timeline({scrollTrigger:{
-//     trigger: ".part-2",
-//     start:"50% 50%",
-//     end:"100% 50%",
-//     scrub:true,
-//     // markers:true,
-//     pin:true,
-// }})
-// tl2.to(".wrapper-div",{
-//     height: 0,
-//     marginTop: 0,
-// })
-// var tl = gsap.timeline({scrollTrigger:{
-//     trigger : ".part-1",
-//     start : "50% 50%",
-//     end : "150% 50%",
-//     markers : true,
-//     pin : true,
-//     srcub: true,
-
-// }})
-// tl.to(".rotate-div", {
-//     rotate: -15,
-//     scale: 0.8,
-//     srcub: true,
-//     delay: 1
-// },'a')
-// tl.to(".row-1", {
-//     margingTop:"-8%",
-// },'a')
-// tl.to(".row-2", {
-//     margingTop:"-12%",
-// },'a')
-// tl.to(".row-3", {
-//     margingTop:"-15%",
-// },'a')
-// tl.to(".row-4", {
-//     margingTop:"-18%",
-// },'a')
-// tl.to(".row-5", {
-//     margingTop:"-21%",
-// },'a')
